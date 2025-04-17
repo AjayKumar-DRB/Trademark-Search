1f1b13486948d43720dd0c082332218cc5df73d0
Ajay Kumar <96405422+AjayKumar-DRB@users.noreply.github.com>
2025-04-18 01:34:40 +0530

    Update TotalTrademarks.js

--- src/components/TotalTrademarks.js
+++ src/components/TotalTrademarks.js
@@ -11,7 +11,7 @@ const TotalTrademarks = () => {
     <div className={`px-6 pt-4 pb-2 m-1 ${total === 0 ? 'hidden' : ''}`}>
       <p className="text-base font-bold text-gray-600 mb-2">
         About <span className="text-gray-600">{total}</span> Trademarks found for{" "}
-        <span className="text-gray-600">"{input}"</span>
+        <span className="text-gray-600">{input}</span>
       </p>
       <hr className="border-gray-300 border-t-2"/>
     </div>
